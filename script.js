98e49d46606abac306c3afcbfa47af780fc139c9
Saanu Sivathasan <saanu@Mac.communityfibre.co.uk>
2025-08-23 12:57:04 +0100

    add and remove works, added counters, next i want a clear fucntion

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const inputTask = document.getElementById("input-bar");
 const addBtn = document.getElementById("add-btn");
-const inProgressList = document.getElementById("current-tasks")
+const inProgressList = document.getElementById("current-tasks");
+const completedTasks = document.getElementById("inactive-tasks");
+const remainingNum = document.getElementById("remaining");
+const completedNum = document.getElementById("completed");
+
+
+
 
 addBtn.addEventListener("click", createTask);
 
@@ -17,8 +23,26 @@ function createTask() {
     li.textContent = task;
     inProgressList.appendChild(li);
 
+    remainingNum.textContent = inProgressList.children.length;
+
     inputTask.value = "";
 
-
+    
+    
 
 }
+
+
+inProgressList.addEventListener("click", completeTask);
+
+function completeTask(){
+    const completedTask = event.target;
+    completedTask.classList.remove("active");
+    completedTask.classList.add("inactive");
+    completedTasks.appendChild(completedTask);
+
+    remainingNum.textContent = inProgressList.children.length;
+    completedNum.textContent = completedTasks.children.length;
+    
+}
+
